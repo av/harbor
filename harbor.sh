8669a74521dabe8d881422c96d9bedaa0c95000f
Ivan Charapanau <mail@av.codes>
2024-09-02 14:37:08 +0200

    chore: v0.1.10 bump

--- harbor.sh
+++ harbor.sh
@@ -2148,7 +2148,7 @@ run_comfyui_command() {
 # == Main script
 # ========================================================================
 
-version="0.1.9"
+version="0.1.10"
 harbor_repo_url="https://github.com/av/harbor.git"
 delimiter="|"
 scramble_exit_code=42
