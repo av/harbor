9d1c78e71882de1c9b50bbe2f51ed8e3daf0ceef
Ivan Charapanau <mail@av.codes>
2024-09-30 22:20:11 +0200

    fix: naming convention for bionicgpt service

--- bionicgpt/start_envoy.sh
+++ bionicgpt/start_envoy.sh
@@ -11,8 +11,8 @@ cp /etc/envoy/envoy.yaml /tmp/envoy.yaml
 # new service names.
 
 # Use sed to replace the addresses in the copied configuration file
-sed -i 's/address: app/address: bi-app/' /tmp/envoy.yaml
-sed -i 's/address: barricade/address: bi-barricade/' /tmp/envoy.yaml
+sed -i 's/address: app/address: bionicgpt-app/' /tmp/envoy.yaml
+sed -i 's/address: barricade/address: bionicgpt-barricade/' /tmp/envoy.yaml
 
 # Start Envoy with the modified configuration
 /usr/local/bin/envoy -c /tmp/envoy.yaml
