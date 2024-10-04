2b147bdd3c59cb4592932c59ccf66950bb50362d
Ivan Charapanau <mail@av.codes>
2024-10-05 00:32:47 +0200

    chore: v0.2.6 bump

--- harbor.sh
+++ harbor.sh
@@ -3387,7 +3387,7 @@ run_repopack_command() {
 # ========================================================================
 
 # Globals
-version="0.2.5"
+version="0.2.6"
 harbor_repo_url="https://github.com/av/harbor.git"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
