54c3036cfb02d6ad4aba03d7b959878bc81d47c0
Ivan Charapanau <mail@av.codes>
2024-11-09 22:46:14 +0100

    chore: v0.2.16 bump

--- harbor.sh
+++ harbor.sh
@@ -3600,7 +3600,7 @@ run_k6_command() {
 # ========================================================================
 
 # Globals
-version="0.2.15"
+version="0.2.16"
 harbor_repo_url="https://github.com/av/harbor.git"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
