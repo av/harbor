044c2b6e1ca39e2988f3f7bc8b8d5549644b2bd2
Ivan Charapanau <mail@av.codes>
2025-01-18 16:29:50 +0100

    chore: v0.2.23

--- harbor.sh
+++ harbor.sh
@@ -3888,7 +3888,7 @@ run_kobold_command() {
 # ========================================================================
 
 # Globals
-version="0.2.22"
+version="0.2.23"
 harbor_repo_url="https://github.com/av/harbor.git"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
