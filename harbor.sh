a766f857703eed796cab8ebf43c68c68eb0fc5fe
Ivan Charapanau <mail@av.codes>
2025-03-01 23:47:22 +0100

    chore: v0.3.0

--- harbor.sh
+++ harbor.sh
@@ -4085,7 +4085,7 @@ run_gptme_command() {
 # ========================================================================
 
 # Globals
-version="0.2.28"
+version="0.3.0"
 harbor_repo_url="https://github.com/av/harbor.git"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
