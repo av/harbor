759253066be81620a09f967321ebdc3813a6b908
Ivan Charapanau <mail@av.codes>
2025-03-08 10:27:08 +0100

    feat: wordgraph webui artifact

--- .scripts/terser.sh
+++ .scripts/terser.sh
@@ -2,4 +2,5 @@
 
 set -eo pipefail
 
-npx html-minifier-terser --collapse-whitespace --remove-comments --minify-css true --minify-js true ./boost/src/custom_modules/artifacts/tokens.html -o ./boost/src/custom_modules/artifacts/tokens_mini.html
+# npx html-minifier-terser --collapse-whitespace --remove-comments --minify-css true --minify-js true ./boost/src/custom_modules/artifacts/tokens.html -o ./boost/src/custom_modules/artifacts/tokens_mini.html
+npx html-minifier-terser --collapse-whitespace --remove-comments --minify-css true --minify-js true ./boost/src/custom_modules/artifacts/graph.html -o ./boost/src/custom_modules/artifacts/graph_mini.html
--- boost/src/custom_modules/webui_artifact.py
+++ boost/src/custom_modules/webui_artifact.py
@@ -6,13 +6,14 @@ import os
 ID_PREFIX='webui_artifact'
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-artifact_path = os.path.join(current_dir, 'artifacts', 'tokens_mini.html')
-
-with open(artifact_path, 'r') as file:
-  artifact = file.read()
+# artifact_path = os.path.join(current_dir, 'artifacts', 'tokens_mini.html')
+artifact_path = os.path.join(current_dir, 'artifacts', 'graph_mini.html')
 
 async def apply(chat: 'ch.Chat', llm: 'llm.LLM'):
+  with open(artifact_path, 'r') as file:
+    artifact = file.read()
+
   await llm.emit_artifact(artifact.replace('<<listener_id>>', llm.id))
-  await asyncio.sleep(0.1)
+  await asyncio.sleep(1.0)
 
   await llm.stream_final_completion()
