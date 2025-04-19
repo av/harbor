0e0e8a87ebedcd9086ed8ac944e4f3f85603cca7
Ivan Charapanau <mail@av.codes>
2025-04-19 15:14:49 +0200

    chore: v0.3.8

--- harbor.sh
+++ harbor.sh
@@ -4183,7 +4183,7 @@ run_mcp_command() {
 # ========================================================================
 
 # Globals
-version="0.3.7"
+version="0.3.8"
 harbor_repo_url="https://github.com/av/harbor.git"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
