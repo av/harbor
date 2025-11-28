be599eda8ed03f9de2f9f58b7008eababe15e5a3
Ivan Charapanau <mail@av.codes>
2025-11-29 00:04:13 +0100

    chore: v0.3.23

--- harbor.sh
+++ harbor.sh
@@ -4479,7 +4479,7 @@ run_modularmax_command() {
 # ========================================================================
 
 # Globals
-version="0.3.22"
+version="0.3.23"
 harbor_repo_url="https://github.com/av/harbor.git"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
