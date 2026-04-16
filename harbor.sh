4396c6ca0f5c6722a1e6b30c731ef5d4031e03f2
av <mail@av.codes>
2026-04-17 01:31:20 +0200

    chore: v0.4.10

--- harbor.sh
+++ harbor.sh
@@ -5332,7 +5332,7 @@ run_modularmax_command() {
 # ========================================================================
 
 # Globals
-version="0.4.9"
+version="0.4.10"
 harbor_repo_url="https://github.com/av/harbor.git"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
