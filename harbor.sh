f82b05635263e23073822a818c3275da5c8a0d7d
av <mail@av.codes>
2026-09-19 01:22:49 +0200

    chore: v0.5.12

--- harbor.sh
+++ harbor.sh
@@ -12439,7 +12439,7 @@ run_modularmax_command() {
 # ========================================================================
 
 # Globals
-version="0.5.11"
+version="0.5.12"
 harbor_release_url="https://api.github.com/repos/av/harbor/releases/latest"
 delimiter="|"
 scramble_exit_code=42
